7e01ad933fef27b725ec019cd1ad72bb4c15dcd1
tomeydev <ptomey.dev@gmail.com>
2026-01-01 19:14:14 -0300

    feat(types): add Project interface to define project structure

---
+++ src/types/Project.ts
@@ -0,0 +1,9 @@
+export interface Project {
+    name: string;
+    description: string;
+    image: string;
+    technologies: string[];
+    repo: string;
+    demo: string;
+    plataform: string;
+}
