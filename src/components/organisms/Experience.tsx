bf271cedaf50e59a950db6ebdefa3bbe2222e068
tomeydev <ptomey.dev@gmail.com>
2026-01-10 11:34:45 -0300

    feat(experience): add experience data to JSON file and update Experience component

--- src/components/organisms/Experience.tsx
+++ src/components/organisms/Experience.tsx
@@ -1,19 +1,11 @@
 import "../../App.css"
 import Title from "../atoms/Title"
+import experienceData from "../../data/experience.json"
 import { Experience as ExperienceType } from "../../types/Experience"
 import ExperienceItem from "../molecules/ExperienceItem"
 
 export default function Experience() {
-    const experiences: ExperienceType[] = [
-        {
-            role: "Mobile Engineer",
-            company: "Tech Solutions",
-            companyUrl: "https://google.com",
-            period: "Jan 2020 - Present",
-            description:"Desarrollé aplicaciones móviles nativas y cross-platform, mejorando la experiencia del usuario y optimizando el rendimiento.",
-            technologies: ["Swift", "Kotlin", "React Native", "Flutter"],
-        },
-    ]
+    const experiences: ExperienceType[] = experienceData;
 
     return (
         <section id="experience" className="py-24 px-6 lg:px-8 relative overflow-hidden">
